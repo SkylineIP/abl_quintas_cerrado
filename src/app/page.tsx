b1600e0b1544d8d0725cb043a34dacb93b39c14a
Matheus Santos <matheussantos.skyline@gmail.com>
2025-10-17 10:29:25 -0300

    -fix redirect homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,17 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import * as React from "react";
-import { useContextDefault } from "@/context/Context";
-import AnimatedText from "./animations/animatedText";
+import { redirect } from "next/navigation";
+import React, { useEffect } from "react";
 
 const Home: React.FC = () => {
-  const router = useRouter();
-  const context = useContextDefault();
-  const setSubmenuAndSelected = context?.setSubmenuAndSelected;
-
-  const handleClick = () => {
-    setSubmenuAndSelected?.("/quintas-cerrado", ""); // Set state before navigation
-    router.push("/quintas-cerrado"); // Redireciona para a página de localização
-  };
+  useEffect(() => {
+    redirect("/quintas-cerrado");
+  }, [])
 
   return (
-    <div
-      className="w-fulll h-screen bg-institucional overflow-hidden min-h-[800px] min-w-[1200px] flex justify-center items-center"
-      onClick={handleClick}
-    >
-      <AnimatedText text="Crie sua primeira experiência" />
-    </div>
+    <>
+
+    </>
   );
 };
 
